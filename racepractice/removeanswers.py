f7c79c955234a6de7f1877b2495dac4357909408
Naama Paulemont <60440166+junaama@users.noreply.github.com>
2023-01-28 01:15:47 -0500

    remove race answers script

---
+++ racepractice/removeanswers.py
@@ -0,0 +1,18 @@
+import re
+
+def remove_answers(filepath):
+    # Open the file for reading
+    with open(filepath, 'r') as f:
+        # Read the file's contents into a list of lines
+        lines = f.readlines()
+
+    # Use a regular expression to find lines that contain answers
+    pattern = re.compile(r'\[Answers\]:.*')
+    lines_without_answers = [line for line in lines if not pattern.search(line)]
+
+    # Open the file for writing
+    with open(filepath, 'w') as f:
+        # Write the lines without answers back to the file
+        f.writelines(lines_without_answers)
+
+remove_answers("race4.txt")
